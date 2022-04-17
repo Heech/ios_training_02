891a7dfa3195d6daf62dce68149c562c840a65e6
“Christofer <christofer.henriksson@monstar-lab.com>
2022-04-17 13:04:41 +0900

    Initial Commit

---
+++ SwiftUi_Training_02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUi_Training_02
+//
+//  Created by Christofer Henriksson on 2022/04/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUi_Training_02/SwiftUi_Training_02App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_Training_02App.swift
+//  SwiftUi_Training_02
+//
+//  Created by Christofer Henriksson on 2022/04/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_Training_02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
